3ddab00374cb384eccef14baa1f3b38f6b7a7557
Yahya Achrane <yahya.achrane15@gmail.com>
2023-07-25 17:40:09 -0700

    test

--- execute.c
+++ execute.c
@@ -1,42 +1,41 @@
 #include "shell.h"
 
 /**
- * execute - Executes a command in a child process and waits for it to complete
- * @line: The command to be executed
- * Return: The exit status of the executed command
- */
-int execute(char **line)
+* execute - Executes a command in a child process and waits for it to complete
+* @line: The command to be executed
+* @av: An array of strings containing the command and its arguments 
+* @env: An array of strings containing the environment variables
+* @args: An array of strings containing the command and its arguments 
+* Return: The exit status of the executed command
+*/
+int execute(char *line, char **av, char **env, char **args)
 {
 	pid_t pid;
 	int status;
-	char *path;
-
 	pid = fork();
+	
 	if (pid == 0)
 	{
+		if (args != NULL)
+	{
+		execve(line, args, env);
+	}
+	else
+	{
+		execve(line, av, env);
+	}
 
-		if (line)
-		{
-			path = get_path(line[0]);
-			if (execve(path, line, NULL) == -1)
-			{
-				perror("execve");
-				return (1);
-			}
-			return (0);
-		}
-		exit(1);
+	exit(1);
 	}
 	else if (pid < 0)
 	{
-		perror("fork");
 		return (1);
 	}
 	else
 	{
 		do
 		{
-			status = waitpid(pid, NULL, WUNTRACED);
+		status = waitpid(pid, NULL, WUNTRACED);
 		} while (status == -1 && errno == EINTR);
 
 		return (status);
@@ -44,12 +43,12 @@ int execute(char **line)
 }
 
 /**
- * ft_substr - Extracts a substring from a given string
- * @s: Pointer to the source string
- * @start: The starting index of the substring
- * @len: The maximum length of the substring
- * Return: A newly allocated string containing the extracted substring
- */
+* ft_substr - Extracts a substring from a given string
+* @s: Pointer to the source string
+* @start: The starting index of the substring
+* @len: The maximum length of the substring
+* Return: A newly allocated string containing the extracted substring
+*/
 
 char *ft_substr(char *s, unsigned int start, size_t len)
 {
@@ -76,30 +75,30 @@ char *ft_substr(char *s, unsigned int start, size_t len)
 }
 
 /**
- * _strlcpy - Safely copies a string to a destination buffer with a limited size
- * @dst: Pointer to the destination buffer
- * @src: Pointer to the source string to be copied
- * @dstsize: Size of the destination buffer
- * Return: The total length of the source string (not including the null-terminator)
- */
+* _strlcpy - Safely copies a string to a destination buffer with a limited size
+* @dst: Pointer to the destination buffer
+* @src: Pointer to the source string to be copied
+* @dstsize: Size of the destination buffer
+* Return: The total length of the source string (not including the null-terminator)
+*/
 size_t _strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	size_t i;
-	size_t l;
+    size_t i;
+    size_t l;
 
-	i = 0;
-	l = strlen(src);
-	if (dst == src)
-		return (l);
-	if (dst == NULL && src == NULL)
-		return (0);
-	if (dstsize == 0)
-		return (l);
-	while (src[i] && (i < dstsize - 1))
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (l);
+    i = 0;
+    l = strlen(src);
+    if (dst == src)
+        return (l);
+    if (dst == NULL && src == NULL)
+        return (0);
+    if (dstsize == 0)
+        return (l);
+    while (src[i] && (i < dstsize - 1))
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    return (l);
 }
--- shell.h
+++ shell.h
@@ -12,29 +12,9 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <stddef.h>
-#include <signal.h>
 
-#define READ_SIZE 1024
-#define BUFSIZE 1024
-
-ssize_t _getline(char **buf, size_t *size_buf, int fild);
-
-extern char **environ;
-
-int execute(char **line);
+int execute(char *line, char **av, char **env, char **args);
 char *ft_substr(char *s, unsigned int start, size_t len);
 size_t _strlcpy(char *dst, const char *src, size_t dstsize);
-char **create_tokens(char *line);
-char *get_path(char *input);
-int builtin_exit(char **args);
-int builtin_env(char **args);
-int builtin_cd(char **args);
-int count_env(void);
-void free_array(char **array);
-void sig_Handler(int sig_num);
-
-char *_strtok(char *str, const char *dem);
-char *_strpbrk(char *str, const char *set);
-int _strspn(char *str, const char *set);
 
 #endif
